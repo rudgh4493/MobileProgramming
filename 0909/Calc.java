a00974713c5309c6acbb6db52aa85ac46b2a40f0
rudgh4493 <70314961+rudgh4493@users.noreply.github.com>
2024-09-09 13:50:43 +0900

    Add files via upload

---
+++ 0909/Calc.java
@@ -0,0 +1,8 @@
+package com.example.test;
+
+public class Calc {
+
+    public static int add(int x, int y) {
+        return x+y;
+    }
+}
---
+++ 0909/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    int total;//field
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int total2; //local
+
+    Calc.add(11,22);
+
+        double degree=2.34;
+        int a[]=new int[3];
+        a[0]=12;
+
+    }
+}
