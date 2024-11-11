242c36a0aa05b35b52d4fe41e92f7faea44d0ca4
rudgh4493 <70314961+rudgh4493@users.noreply.github.com>
2024-11-11 13:51:31 +0900

    Add files via upload

---
+++ 1111/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import static com.example.myapplication.R.*;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RadioGroup radiogroup=findViewById(R.id.radiogroup);
+        CheckBox checkbox=findViewById(R.id.checkbox);
+        checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) radiogroup.setVisibility(View.VISIBLE);
+                else radiogroup.setVisibility(View.GONE);
+            }
+        });
+    }
+}
