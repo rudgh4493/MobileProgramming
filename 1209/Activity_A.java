17a46b1735c812b728a4d60354a1d874c5377ffd
rudgh4493 <70314961+rudgh4493@users.noreply.github.com>
2024-12-09 13:51:32 +0900

    Add files via upload

---
+++ 1209/Activity_A.java
@@ -0,0 +1,33 @@
+package com.example.a1209;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.EditText;
+
+
+//010 02 03 04
+
+
+public class Activity_A extends AppCompatActivity {
+    private static final String MEMO = "MEMO";
+    EditText edittext_title, edittext_memo;
+    SharedPreferences pref;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a);
+        edittext_memo=findViewById(R.id.edittext_memo);
+        pref=getSharedPreferences("myPref", MODE_PRIVATE);
+        if(pref.contains(MEMO)) edittext_memo.setText(pref.getString(MEMO, ""));
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SharedPreferences pref=getSharedPreferences("myPref", MODE_PRIVATE);
+        SharedPreferences.Editor editor=pref.edit();
+        editor.putString(MEMO, edittext_memo.getText().toString());
+        editor.apply();
+    }
+}
